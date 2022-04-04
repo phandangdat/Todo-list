b37528c4313148dd054a77688ad9efe85fff043c
Dang Dat <Dang Dat>
2022-04-04 23:25:57 +0700

    delete todo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,25 +14,17 @@ form.addEventListener('submit', function(event) {
 })
 function addTodoElement(todo) {
     var li = document.createElement('li');
-    li.setAttribute('class', 'item-todo general-size')
-
+    
     li.innerHTML = `
         <span>${todo.text}</span>
+        <span><i class="fa fa-times-circle" aria-hidden="true"></i></span>
     `
-    todos.appendChild(li)
-    li.addEventListener('click', function() {
-        this.classList.toggle("completed");
+    li.setAttribute('class', 'item-todo general-size')
+    li.querySelector('span:first-child').addEventListener('click', function(e) {
+        this.classList.toggle('completed')
     })
+    li.querySelector('span:last-child').addEventListener('click', function(e) {
+        this.parentElement.remove()
+    })
+    todos.appendChild(li)
 }
-// function todoActive {
-//     const buttonAll = querySelector('#all');
-//     const buttonActive = querySelector('#active');
-//     const buttonCompleted = querySelector('#completed');
-
-
-//     buttonAll.onclick = function(e) {
-//         var itemTodos = document.querySelector('.item-todo')
-
-//         // itemTodos.forEach((itemTodo)=>  )
-//     }
-// }
